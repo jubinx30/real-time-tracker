b8f856245ae1ab3b666af0a6cb039376bcf838b2
JX30 <jubinx30@gmail.com>
2026-08-25 13:01:45 +0530

    add the geolocation

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,18 @@
  const socket = io();
+
+ if(navigator.geolocation){
+    navigator.geolocation.watchPosition(
+        (position)=>{
+            const {latitude, longitude} = position.coords;
+            socket.emit("send-location",{latitude,longitude});
+        },
+        (error)=>{
+            console.log(error);
+        },
+        {
+            enableHighAccuracy:true,
+            timeout: 5000,
+            maximumAge: 0,
+        }
+    )
+ }
